49677b3945b0f04b86bd8eeccb211e6a1b42aed6
sergio iribe <sergio.iribec@gmail.com>
2024-08-13 12:16:55 -0700

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,51 @@
-import { useReducer } from 'react';
-import React from 'react';
-import { ToDoList } from './ToDoList';
-import { v4 as uuidv4 } from 'uuid';
+import { useReducer } from 'react'
+import React from 'react'
+import { ToDoList } from './ToDoList'
+import { v4 as uuidv4 } from 'uuid'
 
-export const TodosContext = React.createContext();
+export const TodosContext = React.createContext()
 
-  // Estado inicial para los todos
-  const todosInitialState = {
-    todos: [
-      { id: 1, text: 'finishing writing hooks chapter' },
-      { id: 2, text: 'play with kids' },
-      { id: 3, text: 'read bible' }
-    ]
-  };
+// Estado inicial para los todos
+const todosInitialState = {
+  todos: []
+}
 
-  // Función reductora para manejar las acciones
-  function todosReducer(state, action) {
-    switch (action.type) {
-      case 'delete': 
-        const filteredTodoState = state.todos.filter(todo => todo.id !== action.payload.id)
-        return {...state, todos: filteredTodoState};
-      case 'add':
-        const newToDo = {
-          id: uuidv4(),
-          text: action.payload.text
-        }
-        const addedToDos = [...state.todos, newToDo]
-        return {...state, todos: addedToDos}
-      default:
-        return todosInitialState; // Asegúrate de retornar el estado actual por defecto
-    }
+// Función reductora para manejar las acciones
+function todosReducer(state, action) {
+  switch (action.type) {
+    case 'get':
+      return { ...state, todos: action.payload }
+    case 'delete':
+      const filteredTodoState = state.todos.filter(
+        (todo) => todo.id !== action.payload.id
+      )
+      return { ...state, todos: filteredTodoState }
+    case 'add':
+      const addedToDos = [...state.todos, action.payload] // Usar action.payload en lugar de newToDo
+      return { ...state, todos: addedToDos }
+    case 'edit':
+      const updatedToDo = { ...action.payload }
+      const updatedToDoIndex = state.todos.findIndex(
+        (todo) => todo.id === action.payload.id
+      )
+      const updatedToDos = [
+        ...state.todos.slice(0, updatedToDoIndex),
+        updatedToDo,
+        ...state.todos.slice(updatedToDoIndex + 1)
+      ]
+      return { ...state, todos: updatedToDos }
+    default:
+      return state // Asegúrate de retornar el estado actual por defecto
   }
+}
 
 export const App = () => {
-
-
   // Hook useReducer para manejar el estado de los todos
-  const [state, dispatch] = useReducer(todosReducer, todosInitialState);
+  const [state, dispatch] = useReducer(todosReducer, todosInitialState)
 
   return (
     <TodosContext.Provider value={{ state, dispatch }}>
       <ToDoList />
     </TodosContext.Provider>
-  );
-};
+  )
+}
--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -1,20 +1,50 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useContext } from 'react'
 import { TodosContext } from './App'
+import { useAPI } from './useAPI'
+import axios from 'axios'
+import { v4 as uuidv4 } from 'uuid'
 
 
 export const ToDoList = () => {
   const { state, dispatch } = useContext(TodosContext)
   const [todoText, setTodoText] = useState('')
+  const [editMode, setEditMode] = useState(false)
+  const [editTodo, setEditTodo] = useState(null)
+  const buttonTitle  = editMode? 'Edit' : 'Add'
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    dispatch({ type: 'add', payload: { text: todoText}})
+  const endpoint = "http://localhost:3000/todos/"
+  const savedTodos = useAPI(endpoint)
+
+
+  useEffect(() => {
+    dispatch({type: 'get', payload: savedTodos})
+  }, [savedTodos])
+
+  const handleSubmit = async event => {
+    event.preventDefault()
+    if (todoText.trim() === '') {
+      // Puedes mostrar un mensaje de error aquí o simplemente retornar para evitar el submit
+      alert('Please enter a to-do item before submitting.'); // Ejemplo de mensaje de error
+      return;
+    }
+  
+    if(editMode){
+      await axios.patch(endpoint + editTodo.id, {text: todoText})
+      dispatch({type: 'edit', payload: {...editTodo, text: todoText}})
+      setEditMode(false)
+      setEditTodo(null)
+    } else{
+      const newToDo = {id: uuidv4(), text:todoText}
+      const response = await axios.post(endpoint, newToDo)
+      dispatch({type: 'add', payload: response.data})
+      
+    }
     setTodoText('')
   }
 
   return (
-    <div className="flex w-[50vw] h-[100vh] bg-white m-auto">
+    <div className="flex w-[90vw] md:w-[50vw] h-[100vh] bg-white m-auto">
       <div className='flex flex-col w-full h-[100vh]'>
       <div className='mt-10'>
         <form onSubmit={handleSubmit} className='flex'>
@@ -25,40 +55,43 @@ export const ToDoList = () => {
                 value={todoText}
                 onChange={(e) => setTodoText(e.target.value)}
             />
-            <button className="bg-black text-white rounded-r w-1/4 p-2" onClick={(e) => {
-                e.preventDefault()
-                dispatch({ type: 'add', payload: { text: todoText } })
-                setTodoText('')
-                }}>
-                Submit
+            <button className="text-white rounded-r w-1/4 p-2" style={{ backgroundColor: editMode ? '#6B7280' : 'black' }} onClick={handleSubmit}>
+                {buttonTitle}
                 
                 
             </button>
         </form>
       </div>
 
-      <div className="border-2 border-black mt-5 rounded">
+      <div className="border-2 border-black mt-5 rounded w-[100%]">
         <div className=" bg-white">
           <div className="flex justify-between bg-black p-4">
-            <div className="w-1/2 text-white font-bold">To Do</div>
-            <div className="w-1/4 text-white font-bold">Edit</div>
-            <div className="w-1/4 text-white font-bold">Delete</div>
+            <div className="lg:w-3/4 text-white font-bold">To Do</div>
+            <div className='lg:w-1/4 text-white font-bold flex gap-1 lg:gap-10'>
+            <div className="text-white font-bold ms-3" >Edit</div>
+            <div className="text-white font-bold ">Delete</div>
+            </div>
           </div>
           {state.todos.map((todo) => (
             <div
               key={todo.id}
-              className="flex justify-between p-4 border border-b border-black items-center "
+              className="flex p-4 border border-b border-black items-center w-[100%]"
             >
-              <div className="w-1/2 text-wrap text-ellipsis overflow-hidden ">{todo.text}</div>
-              <div className="w-1/4">
-                <button className="bg-black text-white px-3 py-2 rounded">
+              <div className="w-3/4 break-all">{todo.text}</div>
+              <div className='w-1/4 flex-col lg:flex-row lg:gap-5 gap-1 flex items-center'>
+                <button className="bg-gray-500 text-white px-3 py-2 rounded w-[70px]" onClick={() => {
+              setTodoText(todo.text)
+              setEditMode(true)
+              setEditTodo(todo)
+            }}>
                   Edit
                 </button>
-              </div>
-              <div className="w-1/4">
                 <button
-                  className="bg-black text-white px-3 py-2 rounded"
-                  onClick={() => dispatch({ type: 'delete', payload: todo })}
+                  className="bg-gray-500 text-white px-3 py-2 rounded w-[70px]"
+                  onClick={ async () => {
+                    await axios.delete(endpoint + todo.id)
+                    dispatch({ type: 'delete', payload: todo})
+                  }}
                 >
                   Delete
                 </button>
